f86e2ba78b4ab7c9468e03fc00464336f9a16573
OAKevin <kevinmirandat@gmail.com>
2023-04-19 10:50:35 -0400

    first program and first commit

---
+++ stringCleanup.js
@@ -0,0 +1,4 @@
+const str = 'let s see some spaces';
+const allSpacesRemoved = str.replaceAll(' ', ''); //basically just taking all the spaces and replacing with no space
+
+console.log(allSpacesRemoved); // ABC
